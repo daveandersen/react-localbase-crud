e6755c6a5ac4660417b9891982523694931d86d1
Dave Andersen <daveytheboo@gmail.com>
2021-02-01 11:55:18 +0700

    Shorten function code

--- src/WebWorkers/worker.js
+++ src/WebWorkers/worker.js
@@ -3,98 +3,86 @@ const worker = () => {
         var db;
         var request = indexedDB.open("db");
 
-        switch (e.data.type) {
-            case "Get Username":
-                request.onsuccess = async function (e) {
-                    db = request.result;
+        function getData(type) {
+            console.log("Bohay")
+            request.onsuccess = async function (e) {
+                console.log("Hello")
+                db = request.result;
 
-                    var transaction = db.transaction(["users"], "readwrite");
-                    transaction.oncomplete = function (event) {
-                        console.log("All done!");
-                    };
-
-                    transaction.onerror = function (event) {
-                        // Don't forget to handle errors!
-                    };
-
-                    var objectStore = transaction.objectStore("users");
-                    var request2 = objectStore.getAll();
-                    var usernameArray = [];
-                    request2.onsuccess = function (event) {
-                        event.target.result.forEach(element => {
-                            usernameArray.push({
-                                id: element.id,
-                                username: element.username
-                            })
-                        });
-                        postMessage(usernameArray);
-
-                    }
+                var transaction = db.transaction(["users"], "readwrite");
+                transaction.oncomplete = function (event) {
+                    console.log("All done!");
                 };
-                break;
 
-            case "Get Password":
-                request.onsuccess = async function (e) {
-                    db = request.result;
-
-                    var transaction = db.transaction(["users"], "readwrite");
-                    transaction.oncomplete = function (event) {
-                        console.log("All done!");
-                    };
-
-                    transaction.onerror = function (event) {
-                        // Don't forget to handle errors!
-                    };
-
-                    var objectStore = transaction.objectStore("users");
-                    var request2 = objectStore.getAll();
-                    var passwordArray = [];
-                    request2.onsuccess = function (event) {
-                        event.target.result.forEach(element => {
-                            passwordArray.push({
-                                id: element.id,
-                                password: element.password
-                            })
-                        });
-                        postMessage(passwordArray);
-
-                    }
+                transaction.onerror = function (event) {
+                    // Don't forget to handle errors!
                 };
-                break;
 
-            case "Get CarID":
-                request.onsuccess = async function (e) {
-                    db = request.result;
+                var objectStore = transaction.objectStore("users");
+                var request2 = objectStore.getAll();
+                var objectArray = [];
 
-                    var transaction = db.transaction(["users"], "readwrite");
-                    transaction.oncomplete = function (event) {
-                        console.log("All done!");
-                    };
+                switch (type) {
+                    case "Get Username":
+                        console.log("Hi")
+                        request2.onsuccess = function (event) {
+                            event.target.result.forEach(user => {
+                                objectArray.push({
+                                    id: user.id,
+                                    username: user.username
+                                })
+                            });
+                            postMessage(objectArray);
+                        }
+                    break;
+                    
+                    case "Get Password":
+                        console.log("Hi")
+                        request2.onsuccess = function (event) {
+                            event.target.result.forEach(user => {
+                                objectArray.push({
+                                    id: user.id,
+                                    password: user.password
+                                })
+                            });
+                            postMessage(objectArray);
+                        }
+                    break;
 
-                    transaction.onerror = function (event) {
-                        // Don't forget to handle errors!
-                    };
+                    case "Get CarID":
+                        console.log("Hi")
+                        request2.onsuccess = function (event) {
+                            event.target.result.forEach(user => {
+                                objectArray.push({
+                                    id: user.id,
+                                    carID: user.carID
+                                })
+                            });
+                            postMessage(objectArray);
+                        }
+                    break;
+                }
+            }
+        }
 
-                    var objectStore = transaction.objectStore("users");
-                    var request2 = objectStore.getAll();
-                    var carIDArray = [];
-                    request2.onsuccess = function (event) {
-                        event.target.result.forEach(element => {
-                            carIDArray.push({
-                                id: element.id,
-                                carID: element.carID
-                            })
-                        });
-                        postMessage(carIDArray);
-                    }
-                };
-                break;
+            switch (e.data.type) {
+                case "Get Username":
+                    getData("Get Username");
+                    break;
 
-            default:
-                console.log("Hello from worker.js");
-                self.postMessage()
-                break;
+                case "Get Password":
+                    getData("Get Password");
+                    break;
+
+                case "Get CarID":
+                    getData("Get CarID");
+                    break;
+
+                default:
+                    console.log("Hello from worker.js");
+                    self.postMessage()
+                    break;
+            }
         }
     }
-}
-export default worker;
+    export default worker;
